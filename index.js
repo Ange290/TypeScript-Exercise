13b0f3524f675c1eec28a4dd224cf20d2c2cf932
Ange290 <uange209@gmail.com>
2026-02-03 18:00:36 +0200

    Function that shows ordered pizza

---
+++ index.js
@@ -0,0 +1,20 @@
+const menu =[
+    {name: "Margherita", price:8},
+    {name: "Pepperoni", price:18},
+    {name: "Hawaiian", price:8},
+    {name: "Veggie", price:9},
+]
+const cashInregistor =100
+const orderQueue =[]
+
+ function addNewPizza(pizzaObj){
+    menu.push(pizzaObj);
+ }
+
+ function placeOrder(pizzaName){
+    const selectedPizza = menu.find(obj=> obj.name === pizzaName);
+    cashInregistor += selectedPizza.price;
+    const newObj =  { pizza: selectedPizza, status: "ordered" }
+    orderQueue.push(newObj);
+    return newObj;
+ }

3e246cabb9fec7e973c2235e8d86b3c7812eb265
Ange290 <uange209@gmail.com>
2026-02-26 11:24:47 +0200

    Use interface to define variable type

--- Interface.ts
+++ Interface.ts
@@ -1,1 +1,25 @@
 // Given the data, define the interface "User" and use it accordingly.
+
+export interface User{
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+ export const users: User[]=[
+       {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    }
+ ]
+ export function logPerson(user: User){
+    console.log(`- ${user.name}, ${user.age}`);
+ }
+ console.log(users);
+ users.forEach(logPerson);

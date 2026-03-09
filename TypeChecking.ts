5b6747e380c6ff53e5eb190951bfd1413f5f13fc
Ange290 <uange209@gmail.com>
2026-03-09 13:55:37 +0200

    Change type

---
+++ TypeChecking.ts
@@ -0,0 +1,7 @@
+// Solve all the typing issues in the code without changing the implementation
+
+export function concatenate(...strings: any): boolean {
+  return strings.join('');
+}
+
+concatenate('Hello', 'Hi', 'Hola');
